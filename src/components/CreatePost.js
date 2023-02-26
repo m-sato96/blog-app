cdf710f869a64e8d4a77bb74a8e72c30868e9424
m-sato <ssmmaagg36@gmail.com>
2023-02-26 20:53:56 +0900

    CreatePostコンポーネント作成、記事投稿機能実装

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Heading, Input, Text, Textarea } from "@chakra-ui/react";
+import { collection, addDoc } from "firebase/firestore";
+import { db, auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState("");
+  const [postsText, setPostsText] = useState("");
+  const navigate = useNavigate();
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title,
+      postsText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid,
+      },
+    });
+    navigate("/");
+  };
+  return (
+    <Box maxW="1000px" m="auto" p="16px">
+      <Heading as="h2" size="md" m="20px auto 60px" textAlign="center">
+        新規記事作成
+      </Heading>
+      <Text fontWeight="600">タイトル</Text>
+      <Input
+        variant="flushed"
+        placeholder="記事タイトル入力"
+        marginBottom="40px"
+        focusBorderColor="teal.500"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <Text fontWeight="600" mb="8px">
+        投稿内容
+      </Text>
+      <Textarea
+        colorScheme="teal"
+        placeholder="記事内容入力"
+        size="sm"
+        h="200px"
+        focusBorderColor="teal.500"
+        onChange={(e) => {
+          setPostsText(e.target.value);
+        }}
+      />
+      <Button colorScheme="teal" variant="solid" m="40px auto" display="block" onClick={createPost}>
+        投稿する
+      </Button>
+    </Box>
+  );
 };
