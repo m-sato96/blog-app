1fdbe4b25789c50b518e397e2d337eed75aed604
m-sato <ssmmaagg36@gmail.com>
2023-03-14 22:18:55 +0900

    スタイル調整、meta情報修正、ローディング実装

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { CreatePost } from "./components/CreatePost";
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+const customTheme = extendTheme({
+  fonts: {
+    body: "Noto Sans JP",
+  },
+  styles: {
+    global: {
+      body: {
+        backgroundColor: "#f8f8f8",
+      },
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
